bb939eed60c8380e1d51d423cb87ae93638c9d9f
Ariel Cardoso <arielvix@gmail.com>
2021-04-29 19:18:21 -0300

    Update messages

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.locals.moment = require('moment');
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
